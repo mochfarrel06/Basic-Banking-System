d111d2b682500c06bcd5e357cd3f360b81ee18b3
mochfarrel06 <mochfarrel2606@gmail.com>
2023-10-06 00:46:20 +0700

    Menambahkan metode withDraw pada class BankAccount

--- banking_system.js
+++ banking_system.js
@@ -16,4 +16,24 @@ class BankAccount {
       }, 3000);
     }
   }
+
+  withDraw(amount) {
+    if (this.saldo <= 0) {
+      alert('Saldo anda belum bisa ditarik');
+    } else {
+      amount = parseFloat(prompt('masukkan saldo yang ingin ditarik '));
+      if (isNaN(amount) || amount <= 0) {
+        alert('Masukkan angka yang valid');
+      } else if (amount >= this.saldo) {
+        alert('Saldo anda tidak bisa ditarik');
+      } else {
+        this.saldo -= amount;
+        setTimeout(() => {
+          this.displaySaldo();
+          alert(`Saldo anda adalah ${this.saldo}`);
+        }, 3000);
+        return;
+      }
+    }
+  }
 }
