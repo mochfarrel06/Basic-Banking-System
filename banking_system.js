9e87073679f0fd6d0d0e09f062ea54ce7f9fb58f
mochfarrel06 <mochfarrel2606@gmail.com>
2023-10-06 00:45:26 +0700

    Menambahkan metode deposit pada class BankAccount

--- banking_system.js
+++ banking_system.js
@@ -2,4 +2,18 @@ class BankAccount {
   constructor(saldo) {
     this.saldo = saldo;
   }
+
+  deposit(amount) {
+    amount = parseFloat(prompt('masukkan saldo '));
+    if (isNaN(amount) || amount <= 0) {
+      alert('masukkan angka yang valid');
+      return;
+    } else {
+      this.saldo += amount;
+      setTimeout(() => {
+        this.displaySaldo();
+        alert(`Saldo anda adalah ${this.saldo}`);
+      }, 3000);
+    }
+  }
 }
